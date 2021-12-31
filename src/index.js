e953cb254c75bdc18bd516e81d91937ae941d929
David Ku <hola@davidku.dev>
2021-12-31 11:31:46 -0600

    se instala react-hook-form.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import App from "./App";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 ReactDOM.render(
   <React.StrictMode>
