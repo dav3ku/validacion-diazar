dbfb542230e9babbc1303d6dae85221236efd5ff
David Ku <hola@davidku.dev>
2022-01-14 19:46:31 -0600

    Uso de hashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 
@@ -10,9 +10,9 @@ import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
